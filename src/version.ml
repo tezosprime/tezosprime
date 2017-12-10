40cf141ce492e00e5045ef07501281b6da6e2701
aliibrahim80 <aliibrahim@protonmail.com>
2017-12-10 11:12:19 +0000

    updated version and rebrand for user agent

--- src/version.ml
+++ src/version.ml
@@ -1,8 +1,9 @@
 (* Copyright (c) 2016 The Qeditas developers *)
+(* Copyright (c) 2017 The Dalilcoin developers *)
 (* Distributed under the MIT software license, see the accompanying
    file COPYING or http://www.opensource.org/licenses/mit-license.php. *)
 
-let clientdescr = "Qeditas (alpha)"
-let clientversion = "0.0.1"
+let clientdescr = "Dalilcoin (alpha)"
+let clientversion = "0.0.2"
 let useragent = clientdescr ^ " " ^ clientversion
-let protocolversion = 0l
+let protocolversion = 1l

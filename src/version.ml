b4197ed90188899c5b609af5a92a5a11ee0dda57
aliibrahim80 <aliibrahim@protonmail.com>
2018-03-29 07:45:57 +0000

    changed version number

--- src/version.ml
+++ src/version.ml
@@ -1,9 +1,9 @@
 (* Copyright (c) 2016 The Qeditas developers *)
-(* Copyright (c) 2017 The Dalilcoin developers *)
+(* Copyright (c) 2017-2018 The Dalilcoin developers *)
 (* Distributed under the MIT software license, see the accompanying
    file COPYING or http://www.opensource.org/licenses/mit-license.php. *)
 
 let clientdescr = "Dalilcoin (alpha)"
-let clientversion = "0.0.2"
+let clientversion = "0.0.4"
 let useragent = clientdescr ^ " " ^ clientversion
 let protocolversion = 1l
